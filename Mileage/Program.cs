b722c22c5df9ac0e388a28f022e95950c6123204
Jennifer Young <jclyoung@gmail.com>
2019-03-10 08:49:57 -0500

    Passed

--- Mileage/Program.cs
+++ Mileage/Program.cs
@@ -7,10 +7,24 @@ namespace Mileage
         public static void Main()
         {
             Console.Write("How many miles have you driven? ");
+            string mileageInput = Console.ReadLine();
+            mileageInput = mileageInput.Trim();
 
             // Get input from the user and save it to a variable.
 
             Console.Write("How many gallons of gas have you consumed? ");
+            string gasConsumedInput = Console.ReadLine();
+            gasConsumedInput = gasConsumedInput.Trim();
+
+            int mileage;
+            int.TryParse(mileageInput, out mileage);
+            int gasConsumed;
+            int.TryParse(gasConsumedInput, out gasConsumed);
+
+            int milesPerGallon = (mileage / gasConsumed);
+
+            Console.WriteLine($"Your miles per gallon is {milesPerGallon}");
+
 
             // Get input from the user and save it to a second variable.
 
